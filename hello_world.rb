372b689d20ec6bb80660ef6173d054ae44d0f195
EricMTodd <eterictodd@gmail.com>
2020-03-28 16:29:51 -0600

    Minor formatting changes

--- hello_world.rb
+++ hello_world.rb
@@ -25,26 +25,31 @@ puts "Author's age: #{authors_age}\n\n"
 
 # Gets challenge, section 5.5
 puts gets
-puts "\nWhat's your name? "
-name = gets.chomp
-puts "\nHello #{name}, it's a pleasure to meet you!"
+puts "\nWhat's your first name? "
+first_name = gets.chomp
+puts "\nHello #{first_name}, it's a pleasure to meet you!"
 
 
 # Challenge 5.6
 # Get a person's full name and greet them with it.
-puts "\nWhat's your first name? "
-first_name = gets.chomp
-
 puts "\nWhat's your middle name? "
 middle_name = gets.chomp
 
 puts "\nWhat's your last name? "
 last_name = gets.chomp
 
-puts "\nHello #{first_name} #{middle_name} #{last_name}, it's a pleasure to meet you!\n"
+name_letter_count = first_name.length + middle_name.length + last_name.length
+puts "\nYou have #{name_letter_count} letters in your name, #{first_name}."
 
 # Get someone's favorite number, add one and suggest it as a better favorite number.
 puts "\nWhat's your favorite number?"
 favorite_number = gets.chomp.to_i
 new_favorite_number = favorite_number + 1
 puts "\nMay I recommend your favorite number be #{new_favorite_number}? It is bigger and better than #{favorite_number} after all.\n\n"
+
+
+# String interpolation test.
+# puts "What is your full name?"
+# name = gets.chomp
+# puts "There are #{name.length - 2} letters in your name."
+# Types do not have to be set to string when using this method of puts.
