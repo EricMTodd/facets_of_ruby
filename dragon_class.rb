aa2f6c884d06aad56c3ee92c3e2f5012406fce4f
-thugnasty- <eterictodd@gmail.com>
2020-04-06 16:20:19 -0600

    dragon_class working correctly

--- dragon_class.rb
+++ dragon_class.rb
@@ -4,32 +4,32 @@ class Dragon
         @name = name
         @asleep = false
         @stuff_in_belly = 10
-        @stuff_in_guts = 0
+        @stuff_in_intestine = 0
 
         puts("#{@name} is born...")
     end
 
-    def feed
+    def feed()
         puts("You feed #{@name}.")
-        @stuff_in_belly = 0
-        passage_of_time
+        @stuff_in_belly = 10
+        passage_of_time()
     end
 
-    def walk
-        puts("you walk #{@name}")
-        @stuff_in_guts = 0
-        passage_of_time
+    def walk()
+        puts("You walk #{@name}")
+        @stuff_in_intestine = 0
+        passage_of_time()
     end
 
-    def put_to_bed
+    def put_to_bed()
         puts("You put #{@name} to bed.")
-        @aleep = true
+        @asleep = true
         3.times do
             if @asleep
-                passage_of_time
+                passage_of_time()
             end
             if @asleep
-                puts("#{name} snores softly.")
+                puts("#{@name} snores softly.")
             end
         end
         if @asleep
@@ -38,53 +38,48 @@ class Dragon
         end
     end
 
-    def toss
-        puts("You toss #{@name} up into the air. #{@name} appears to be having fun!")
-        passage_of_time
+    def toss()
+        puts("You toss #{@name} up into the air, they seem to enjoy it!")
+        passage_of_time()
     end
-    def rock
+    def rock()
         puts("You rock #{@name} gently.")
         @asleep = true
-        puts("He briefly dozes off...")
-        passage_of_time
+        puts("#{@name} briefly dozes off...")
+        passage_of_time()
         if @asleep
-            @asleep = falseputs("...but wakes up when you stop.")
+            @asleep = false
+            puts("...but wakes up when you stop.")
         end
     end
 
     private
     def hungry?
-       @stuff_in_belly <= 2 
-    end
-    
-    def poopy?
-        @stuff_in_guts >= 8
+        @stuff_in_belly <= 2
     end
 
-    def passage_of_time
+    def poopy?
+        @stuff_in_intestine >= 8
+    end
+
+    def passage_of_time()
         if @stuff_in_belly > 0
             @stuff_in_belly -= 1
-            @stuff_in_guts += 1
+            @stuff_in_intestine += 1
         else
             if @asleep
                 @asleep = false
                 puts("#{@name} wakes up suddenly!")
             end
-            puts("#{@name} is starving! In desperation, they are you!")
-            exit
+            puts("#{@name} is starving! In desperation, they ate you!")
+            exit()
         end
-        if @stuff_in_guts >= 10
-            @stuff_in_guts = 0
+        if @stuff_in_intestine >= 10
+            @stuff_in_intestine = 0
             puts("Whoops! #{@name} had an accident...")
         end
+
         if hungry?
-            if @asleep
-                @asleep = false
-                puts("#{@name} wakes up suddenly!")
-            end
-            puts("#{name}'s stomach grumbles.")
-        end
-        if poopy?
             if @asleep
                 @asleep = false
                 puts("#{@name} wakes up suddenly!")
@@ -94,6 +89,7 @@ class Dragon
     end
 end
 
+puts
 pet = Dragon.new("Meraxes")
 pet.feed
 pet.toss
